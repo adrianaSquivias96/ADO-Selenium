5d58f60cc6773026349c6e10376499bb4b93d66c
adrianaSquivias96 <adrianasquivias@gmail.com>
2025-03-23 17:25:24 -0600

    refactor

--- seleniumADO/src/test/java/utils/FileManager.java
+++ seleniumADO/src/test/java/utils/FileManager.java
@@ -1,39 +1,44 @@
-
 package utils;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Properties;
 import java.util.Scanner;
 
 public class FileManager {
-    public String resourcesPath = System.getProperty("user.dir") + "\\src\\test\\resources\\";
 
-    public String readFromFile(String filePath){
-        String content = new String();
+
+    public String resourcesPath = System.getProperty("user.dir") + "\\seleniumADO\\src\\test\\resources\\";
+
+
+    public String readFromFile(String filePath) {
+        StringBuilder content = new StringBuilder();
 
         try {
             File myObj = new File(filePath);
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                System.out.println(data);
-                content += data;
+                System.out.println(data);  // Imprimir en consola (puedes eliminarlo si no lo necesitas)
+                content.append(data).append(System.lineSeparator());
             }
             myReader.close();
-            return content;
+            return content.toString();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
             return null;
         }
     }
-    public String writeInFile(String fileName, String content){
-        //Revisar si el archivo existe si no crearlo
-        String filePath = createFile(fileName);
 
-        if(filePath == null) return null;
+
+    public String writeInFile(String fileName, String content) {
+
+        String filePath = createFile(fileName);
+        if (filePath == null) return null;
 
         try {
             FileWriter myWriter = new FileWriter(filePath);
@@ -48,14 +53,14 @@ public class FileManager {
         }
     }
 
-    public String createFile(String fileName){
+    public String createFile(String fileName) {
         try {
             String filePath = resourcesPath + fileName;
             File myObj = new File(filePath);
             if (myObj.createNewFile()) {
                 System.out.println("File created: " + myObj.getName());
             } else {
-                System.out.println("File already exists. " + filePath);
+                System.out.println("File already exists: " + filePath);
             }
             return filePath;
         } catch (IOException e) {
@@ -64,5 +69,36 @@ public class FileManager {
             return null;
         }
     }
-}
 
+
+    public String[] getCredentials() {
+        String username = "";
+        String password = "";
+
+        try {
+
+            FileInputStream fis = new FileInputStream(resourcesPath + "Folio.txt");
+            Properties prop = new Properties();
+            prop.load(fis);
+
+
+            username = prop.getProperty("username");
+            password = prop.getProperty("password");
+
+            return new String[] { username, password };
+        } catch (IOException e) {
+            System.out.println("An error occurred while loading credentials.");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    public void saveCredentialsToFile(String fileName) {
+        String[] credentials = getCredentials();
+        if (credentials != null) {
+            String content = "username=" + credentials[0] + "\npassword=" + credentials[1];
+            writeInFile(fileName, content);
+        }
+    }
+}
--- seleniumADO/src/test/java/utils/TestBase.java
+++ seleniumADO/src/test/java/utils/TestBase.java
@@ -2,9 +2,7 @@ package utils;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.time.Duration;
 import java.util.Properties;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -19,37 +17,32 @@ public class TestBase {
 
     public WebDriver WebDriverManager() throws IOException {
         String url = "";
-        FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "//src//test//resources//global.properties");
+        FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "//seleniumADO//src//test//resources//global.properties");
         Properties prop = new Properties();
         prop.load(fis);
         String env = prop.getProperty("env");
 
-        String username = "";
-        String password = "";
+        // Usamos FileManager para obtener las credenciales del archivo
+        FileManager fileManager = new FileManager();
+        String[] credentials = fileManager.getCredentials();
+        String username = credentials[0];  // username desde Folio.txt
+        String password = credentials[1];  // password desde Folio.txt
 
-        if(env.contains("Drp")){
+        if (env.contains("Drp")) {
             url = prop.getProperty("DrpUrl");
-            username = "customer";
-            password = "VnUyYSZoS9smK77RDXTknvTKdzgbi8";
-        } else if(env.contains("QA")){
+        } else if (env.contains("QA")) {
             url = prop.getProperty("QAUrl");
-            username = "customer";
-            password = "VnUyYSZoS9smK77RDXTknvTKdzgbi8";
-        }else{
+        } else {
             url = prop.getProperty("QAUrl");
-            username = "customer";
-            password = "VnUyYSZoS9smK77RDXTknvTKdzgbi8";
         }
+
         String browser_properties = prop.getProperty("browser");
         String browser_maven = System.getProperty("browser");
         Boolean headless = Boolean.parseBoolean(prop.getProperty("headless"));
         Boolean max = Boolean.parseBoolean(prop.getProperty("max"));
         int implicit = Integer.parseInt(prop.getProperty("implicitWait"));
-        // result = testCondition ? value1 : value2
 
         String browser = browser_maven != null ? browser_maven : browser_properties;
-
-
 
         if (driver == null) {
             if (browser.equalsIgnoreCase("chrome")) {
@@ -61,35 +54,26 @@ public class TestBase {
                     options.addArguments("--headless=new");
                     driver = new ChromeDriver(options);
                 }
-               // options.setHeadless(headless);
-                driver = new ChromeDriver(options);// driver gets the life
-            }
-            if (browser.equalsIgnoreCase("firefox")) {
+                driver = new ChromeDriver(options);  // Chrome driver
+
+            } else if (browser.equalsIgnoreCase("firefox")) {
                 FirefoxOptions options = new FirefoxOptions();
-                //options.addArguments("--remote-allow-origins=*");
                 options.setAcceptInsecureCerts(true);
                 driver = new FirefoxDriver(options);
-            }
 
-            if(browser.equalsIgnoreCase("edge")){
+            } else if (browser.equalsIgnoreCase("edge")) {
                 EdgeOptions options = new EdgeOptions();
                 options.addArguments("--remote-allow-origins=*");
                 options.setAcceptInsecureCerts(true);
                 if (headless) {
                     options.addArguments("--headless=new");
                 }
-               // options.setHeadless(headless);
                 driver = new EdgeDriver(options);
             }
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicit));
-            driver.get("https://" + username + ":" + password + "@" + url);
-
-
+            driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(implicit));
+            driver.get("https://" + username + ":" + password + "@" + url);  // URL con las credenciales
         }
         if (max) driver.manage().window().maximize();
-
         return driver;
-
     }
 }
-
